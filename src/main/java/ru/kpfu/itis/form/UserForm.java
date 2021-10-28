4b6e9ee6300500db063232cde11eac43d2e3e236
BlackPrince163 <4523120100Ni>
2021-10-29 00:05:27 +0300

    + create form

--- src/main/java/ru/kpfu/itis/form/UserForm.java
+++ src/main/java/ru/kpfu/itis/form/UserForm.java
@@ -1,61 +1,20 @@
 package ru.kpfu.itis.form;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserForm {
-    private String login;
-    private String password;
     private String lastName;
     private String firstName;
+    private String email;
+    private String password;
 
-    public UserForm(String login, String password, String lastName, String firstName) {
-        this.login = login;
-        this.password = password;
-        this.lastName = lastName;
-        this.firstName = firstName;
+
     }
-
-    public UserForm() {
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-
-    @Override
-    public String toString() {
-        return "UserForm{" +
-                "login='" + login + '\'' +
-                ", password='" + password + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", firstName='" + firstName + '\'' +
-                '}';
-    }
-}
